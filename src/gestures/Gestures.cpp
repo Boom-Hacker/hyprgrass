455286f0ef40391a02ea359e7c7ccfc838d4ae4f
Ching Pei Yang <badnam3o.0@gmail.com>
2024-12-14 02:11:16 +0100

    fix: faulty event handling

--- src/gestures/Gestures.cpp
+++ src/gestures/Gestures.cpp
@@ -289,9 +289,9 @@ void IGestureManager::addEdgeSwipeGesture(const float* sensitivity, const int64_
     this->addTouchGesture(std::move(gesture));
 }
 
+// TODO: timeouts (also in other gestures)
 void IGestureManager::addPinchGesture(const float* threshold, const int64_t* timeout) {
     auto pinch_begin = std::make_unique<PinchAction>(threshold);
-    /*auto pinch_begin_ptr = pinch_begin.get();*/
 
     auto pinch_wrapper = std::make_unique<OnCompleteAction>(std::move(pinch_begin), [this]() {
         PinchDirection dir = this->m_sGestureState.get_pinch_scale() < 1.0 ? PinchDirection::IN : PinchDirection::OUT;
@@ -309,6 +309,15 @@ void IGestureManager::addPinchGesture(const float* threshold, const int64_t* tim
 
     auto ack = [this]() {
         if (!this->activeDragGesture.has_value()) {
+            auto pinch_direction =
+                this->m_sGestureState.get_pinch_scale() < 1.0 ? PinchDirection::IN : PinchDirection::OUT;
+            auto event =
+                CompletedGestureEvent{CompletedGestureType::PINCH, 0,
+                                      static_cast<int>(this->m_sGestureState.fingers.size()), 0, pinch_direction};
+
+            if (this->emitCompletedGesture(event)) {
+                this->cancelTouchEventsOnAllWindows();
+            }
             return;
         }
 
@@ -316,11 +325,6 @@ void IGestureManager::addPinchGesture(const float* threshold, const int64_t* tim
         if (this->emitDragGestureEnd(active)) {
             return;
         }
-
-        auto event =
-            CompletedGestureEvent{CompletedGestureType::PINCH, 0, active.finger_count, 0, active.pinch_direction};
-
-        this->emitCompletedGesture(event);
     };
     auto cancel = [this]() { this->handleCancelledGesture(); };
 
